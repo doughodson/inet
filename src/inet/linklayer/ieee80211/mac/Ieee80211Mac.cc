dbc69733d210a41ddea91f3a49246ab3c9f018c5
Levente Mészáros <levy@omnetpp.org>
2015-02-06 17:24:02 +0100

    Dropped unused function.

--- src/inet/linklayer/ieee80211/mac/Ieee80211Mac.cc
+++ src/inet/linklayer/ieee80211/mac/Ieee80211Mac.cc
@@ -1427,13 +1427,6 @@ simtime_t Ieee80211Mac::getDIFS(int category)
     }
 }
 
-simtime_t Ieee80211Mac::getHeaderTime(double bitrate)
-{
-    Ieee80211PhyMode modType;
-    modType = Ieee80211Mode::getModulation(opMode, bitrate);
-    return modType.getPreambleAndHeader(wifiPreambleType);
-}
-
 simtime_t Ieee80211Mac::getAIFS(int AccessCategory)
 {
     return AIFSN(AccessCategory) * getSlotTime() + getSIFS();
